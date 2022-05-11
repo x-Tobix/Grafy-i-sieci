28ba4342b1dcf248791d3ff73dae6c72027c7219
Filip Tobiasz (Z6FTB) <ftb@kmd.dk>
2022-05-11 21:55:07 +0200

    Implement method for getting alias edge

--- Node2Vec/Node2Vec.py
+++ Node2Vec/Node2Vec.py
@@ -1,5 +1,4 @@
 import numpy as np
-
 from BaseAlgorithm import BaseAlgorithm
 
 
@@ -8,11 +7,11 @@ class Node2Vec(BaseAlgorithm):
     Main class for Node2Vec algorithm model.
     """
 
-    def __init__(self, adjacency_matrix, l, r, p, q, dimension):
+    def __init__(self, g, adjacency_matrix, le, r, p, q, dimension):
         """
         Node2Vec constructor.
         @param adjacency_matrix: Adjacency matrix representing graph.
-        @param l: The length of a single random walk.
+        @param le: The length of a single random walk.
         @param r: Number of random walks starting at a single vertex.
         @param p: Bias parameter of the random walks (Return).
         @param q: Bias parameter of the random walks (In-Out).
@@ -20,12 +19,33 @@ class Node2Vec(BaseAlgorithm):
         if dimension <= 0:
             raise Exception("Embedding dimension must be bigger than 0.")
         self.S = self.load_adjacency_matrix(adjacency_matrix)
-        self.L = l
+        self.L = le
         self.R = r
         self.P = p
         self.Q = q
         self.d = dimension
+        self.G = g
         super().__init__()
+
+    def get_alias_edge(self, source, other):
+        """
+        Get alias edge based on normalized probabilities.
+        @param source: Source node.
+        @param other: Node on the other end of the edge.
+        :return list: Utility list from normalized probabilities
+        """
+
+        probabilities = []
+        for neighbor in sorted(self.G.neighbors(other)):
+            if neighbor == source:
+                probabilities.append(1 / self.P)
+            elif not self.G.has_edge(neighbor, source):
+                probabilities.append(1 / self.Q)
+            else:
+                probabilities.append(1)
+        normalized_probabilities = [float(probability) / sum(probabilities) for probability in probabilities]
+
+        return self.create_utility_lists(normalized_probabilities)
 
     @staticmethod
     def create_utility_lists(probabilities):
@@ -47,14 +67,14 @@ class Node2Vec(BaseAlgorithm):
 
         while len(smaller) > 0 and len(larger) > 0:
             s = smaller.pop()
-            l = larger.pop()
+            la = larger.pop()
 
-            list2[s] = l
-            list1[l] = list1[l] + list1[s] - 1.0
-            if list1[l] < 1.0:
-                smaller.append(l)
+            list2[s] = la
+            list1[la] = list1[la] + list1[s] - 1.0
+            if list1[la] < 1.0:
+                smaller.append(la)
             else:
-                larger.append(l)
+                larger.append(la)
 
         return list2, list1
 
